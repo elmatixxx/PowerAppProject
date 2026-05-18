e9d25388ee923092c53de807f023963a643362d4
José Contreras <joa.contrerass@duocuc.cl>
2026-05-18 01:16:27 -0400

    Cambio Logro.java

--- powerapp-logros/src/main/java/cl/powerapp/logros/model/Logro.java
+++ powerapp-logros/src/main/java/cl/powerapp/logros/model/Logro.java
@@ -1,17 +1,35 @@
 package cl.powerapp.logros.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity @Table(name = "logros")
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Logro {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message="El nombre no puede estar vacio")
+    @Column(nullable= false)
     private String nombre;
+
+    @NotBlank(message="La descripcion no puede estar vacia")
     private String descripcion;
-    private String icono;
-    private Integer condicionValor;
-    @Enumerated(EnumType.STRING)
-    private TipoCondicion tipoCondicion;
+
+    @Min(0)
+    private Integer puntos;
+
+
+    @NotBlank(message="La condicion no puede estar vacia")
+    private String condicion; // Ej: Rachas en dias
+
 }
